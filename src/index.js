d5ded63e466da975f1da8e10dbd7d91d8cc57ab1
cehcop <galich.andrey@gmail.com>
2022-12-25 19:32:23 +0700

    Improvement: File structure

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import TodoList from './components/todo-list';
-import SearchPanel from './components/search-panel';
-import ItemStatus from './components/item-status-filter';
-
-import "./index.css";
-
-const App = () => {
-
-  const todoData = [
-    {label: 'Drink Tasty Tea', important: false, id: 1},
-    {label: 'Make Awesome App', important: true, id: 2},
-    {label: 'Have a lunch', important: false, id: 3}
-  ];
-
-  // const isLoggedIn = false;
-  // const loginBox = <span>Log in, please</span>;
-  // const welcomeBox = <span>Welcome Back</span>
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo="1" done="3" />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatus />
-      </div>
-      <TodoList todos={todoData} />
-    </div>
-  );
-}
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // { isLoggedIn ? welcomeBox : loginBox }
